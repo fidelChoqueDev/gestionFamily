c02c5ff0065dac299305b94bf3b384e325adacbd
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2025-11-09 14:29:43 +0100

    add css and className in component Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,12 @@ export const Navbar = () => {
   return (
     <nav className="container">
 			<header className="header">
-				<div>
+				<div className="logo-container">
 					<img src={logo} alt="Logo" className="logo" />
 					<h1>Crash Coin</h1>
 				</div>
 				<div>
-					<ul>
+					<ul className="nav-links">
 						{NavbarLinks.map((link) => (
 							<li key={link.id}>
 								<a href={link.path}>{link.name}</a>
