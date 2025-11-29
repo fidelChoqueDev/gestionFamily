9fae4480e769931056f1e9bfd113035ec9348a1c
DevFicho PortStart <devcrash03@gmail.com>
2025-11-30 00:05:09 +0100

    install tailwindcss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-import "./Navbar.css"
+//import "./Navbar.css"
 import logo from "../assets/logoV2.svg"
 import { NavbarLinks } from "../utils/Ruetes"
 
 export const Navbar = () => {
   return (
-    <nav className="container">
-			<header className="header">
-				<div className="logo-container">
+    <nav className="bg-gray-900 p-4">
+			<header className="bg-red ">
+				<div className="bg-green-500">
 					<img src={logo} alt="Logo" className="logo" />
 					<h1>Crash Coin</h1>
 				</div>
 				<div>
-					<ul className="nav-links">
+					<ul className="flex space-x-4">
 						{NavbarLinks.map((link) => (
 							<li key={link.id}>
 								<a href={link.path}>{link.name}</a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className="bg-black">Home</div>
   )
 }
