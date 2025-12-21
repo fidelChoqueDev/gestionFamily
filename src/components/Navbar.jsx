30c8f94a170aa3514a2af6c03ea44c2abf4cda9c
DevFicho PortStart <devcrash03@gmail.com>
2025-12-21 09:50:37 +0100

    add css and class in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,31 @@
 import "./Navbar.css"
 import logo from "../assets/logoV2.svg"
 import { NavbarLinks } from "../utils/Ruetes"
+import { useState } from "react";
 
 export const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="header">
-		<div>
-			<img src={logo} alt="Logo" className="logo" />
-		</div>
-		<h1>Crash Coin</h1>
-		<ul>
-			{NavbarLinks.map((link) => (
-				<li key={link.id}>
-					<a href={link.path}>{link.name}</a>
-				</li>
-			))}
-		</ul>
-		<div className="nav_toggle">
-			<span></span>
-			<span></span>
-			<span></span>
-		</div>
-	</nav>
+			<div>
+				<img src={logo} alt="Logo" className="logo" />
+			</div>
+			<h1>Crash Coin</h1>
+			<div className={`nav_items ${isOpen && "open"}`}>
+				<ul>
+					{NavbarLinks.map((link) => (
+						<li key={link.id}>
+							<a href={link.path}>{link.name}</a>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className= {`nav_toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)}>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</nav>
   )
 };
 
